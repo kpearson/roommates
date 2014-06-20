907387ea7d066f42fb2b6e3285bd8fe4a24dd733
Kit <dev_kit@icloud.com>
2014-06-19 19:16:33 -0700

    Cleaned up Twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,26 +1,27 @@
 require 'twilio-ruby'
- 
+
 class TwilioController < ApplicationController
   include Webhookable
 
   ACCOUNT_SID = ENV["ACCOUNT_SID"]
 	AUTH_TOKEN = ENV["AUTH_TOKEN"]
- 
+
   after_filter :set_header
- 
+
   def voice
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
-        r.Play 'http://linode.rabasa.com/cantina.mp3'
+    response = Twilio::TwiML::Response.new do |response|
+      response.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+        response.Play 'http://linode.rabasa.com/cantina.mp3'
 	  end
 	end
 
   def text
-  	User.where(:id => params[:receivers]).each do |u|
+  	User.where(:id => params[:receivers]).each do |roommates|
+      @body = (params[:message]) << "\n    - #{roommates.name} " "#{roommates.phone_number}"
   		client.account.messages.create(
   			:from => "(818) 926-4291", # phone number REQUIRED by twilio API
   			:to   => u.phone_number,
-  			:body => params[:message]
+  			:body => @body
   		)
   	end
   	redirect_to users_path
@@ -31,4 +32,3 @@ class TwilioController < ApplicationController
   	Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
   end
 end
-
